d771db7776ccacfa5fff8edd7d8ff07484298ecc
Harshal Kolhe <harshalkolhe0@gmail.com>
2024-03-03 18:02:58 +0530

    MultiSelect project

---
+++ MultiSelect/src/config.js
@@ -0,0 +1,22 @@
+export const elements = [
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+        text: "React",
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+        text: "Vue",
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+        text: "Node",
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+        text: "Java",
+    },
+    {
+        imgSrc: "https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg",
+        text: "Javascript",
+    },
+];
